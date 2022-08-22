04bf0fec9842bb5bf79f33d7714b23ff4feefdc7
ayush <ayush@WINTER>
2022-08-22 23:18:39 +0530

    Add project files.

---
+++ OnlineShopWEbAPI/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopWEbAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWEbAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public AppDbContext _context { get; }
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return Ok(_context.Users.ToList());
+        }
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            var data = _context.Users.FirstOrDefault(u => u.UID == id);
+            return (data);
+        }
+        [HttpPost]
+        public ActionResult Post(User newUser)
+        {
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+            //return Ok();
+            return CreatedAtAction("Get", new { newUser.UID }, newUser);
+        }
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, User modifiedCat)
+        {
+            var data = _context.Users.FirstOrDefault(u => u.UID == id);
+            if (data == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                data.Lastname = modifiedCat.Lastname;
+                _context.SaveChanges();
+                return Ok();
+            }
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var data = _context.Users.FirstOrDefault(u=> u.UID == id);
+            _context.Users.Remove(data);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ OnlineShopWEbAPI/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWEbAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int UID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string City { get; set; }
+        public string Pincode { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Confirm_Password { get; set; }
+    }
+}
